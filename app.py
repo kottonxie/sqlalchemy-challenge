d3f505a94ceae2be796450750b743782c489e82e
kottonxie <117491329+kottonxie@users.noreply.github.com>
2023-05-30 20:18:14 -1000

    app.py update

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 # Import the dependencies.
-import numpy as np
+# import numpy as np
 
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
+# import sqlalchemy
+# from sqlalchemy.ext.automap import automap_base
+# from sqlalchemy.orm import Session
+# from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
-
 
 #################################################
 # Database Setup
@@ -21,7 +20,7 @@ Base.prepare(autoload_with=engine)
 
 # Save references to each table
 measurement = Base.classes.measurement
-station = Base.classes.station
+station = Base.classses.station
 
 # Create our session (link) from Python to the DB
 session = Session(engine)
@@ -35,3 +34,31 @@ app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def home():
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/names<br/>"
+        f"/api/v1.0/passengers"
+    )
+
+
+# @app.route("/api/v1.0/precipitation")
+# def index():
+#     return "Hello, world!"
+
+# @app.route("/api/v1.0/stations")
+# def index():
+#     return "Hello, world!"
+
+# @app.route("/api/v1.0/tobs")
+# def index():
+#     return "Hello, world!"
+
+# @app.route("/api/v1.0/<start>")
+# def index():
+#     return "Hello, world!"
+
+# @app.route("/api/v1.0/<start>/<end>")
+# def index():
+#     return "Hello, world!"
